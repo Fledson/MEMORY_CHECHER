c5771f844dadd0bf1c2ef526b938f1cb0660850d
Fledson Henrique <fledsonhenrique@gmail.com>
2021-04-27 23:27:44 -0300

    Ajustes na aplicação

---
+++ logger.js
@@ -0,0 +1,25 @@
+const EventEmitter = require('events');
+const fs = require('fs'); //=> modulo para manipulação de arquivos
+const path = require('path');
+const emitter = new EventEmitter();
+
+emitter.on('log', (message) => {
+  console.log(message);
+});
+
+function log(message) {
+  fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
+    if (err) throw err;
+  })
+}
+
+function returnCurrentDate(){
+  var dNow = new Date();
+  var currentDate = `${dNow.getDate()}/${(dNow.getMonth()+1).toString().padStart(2, "0")}/${dNow.getFullYear()} ${dNow.getHours()}:${dNow.getMinutes()}`
+
+  return currentDate;
+  }
+
+// exportando função
+module.exports = log; 
+module.exports = returnCurrentDate;
--- states.js
+++ states.js
@@ -1,4 +1,6 @@
 const os = require("os");
+const log = require("./logger")
+const returnCurrentDate = require("./logger")
 const { freemem, totalmem } = os;
 
 setInterval(() => {
@@ -6,13 +8,19 @@ setInterval(() => {
   const free = parseInt(freemem() / 1024 / 1024);
   const percent = parseInt((free / total )*100);
 
-  const perfom = {
+  const memoryPerformance = {
     Total: `${total}MB`,
     Livre: `${free}MB`,
-    Usada: `${percent}%`
+    freeMemory: `${percent}%`,
+    memoryUsage: `${100-percent}%`
   }
   
   console.clear();
   console.log("==== DADOS DE MEMORIA RAM ====");
-  console.table(perfom);
+  console.table(memoryPerformance);
+  log(`${JSON.stringify(memoryPerformance)}\n`)
+  if (percent <= 50) {
+    console.log(`Memoria com ${memoryPerformance.memoryUsage} de uso -> DICA: feche alguns programas para manter a instabilidade! -> ${returnCurrentDate()}`);
+    log(` ====> DICA: FECHE ALGUNS PROGRAMAS OU ABAS DO NAVEGADOR CASO ESTEJA USANDO PARA MANTER A INSTABILIDADE -> ${returnCurrentDate()} \n \n`)
+  }
 },1000);
